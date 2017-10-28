6bac098d8eea95f234ed7eaf705dbcaa11868312
AliOgretmen <aliogretmen.ch@gmail.com>
2017-10-28 13:44:18 +0200

    jquery

--- js/maze/index.js
+++ js/maze/index.js
@@ -1,17 +1,22 @@
-$(document).keyup(function (event) {
-  
-    if (event.which == 37) {
-        $(".selected")
-        $(".selected").removeClass("selected");
-        console.log("left");
-    }
-    else if (event.which == 38) {
-        console.log("up");
-    }
-     else if(event.which == 39) {
-        console.log("right");
-    }
-    else if (event.which == 40) {
-        console.log("down");
-    }
-});
+ $(document).keyup(function (event) {
+     $(".selected").removeClass("selected");
+     if (event.which == 37 && column > 1) {
+         console.log("left");
+         column--;
+     }
+     else if (event.which == 38 && row > 1) {
+         console.log("up");
+         row--;
+     }
+     else if (event.which == 39 && column < 3) {
+         console.log("right");
+         column++;
+     }
+     else if (event.which == 40 && row < 3) {
+         console.log("down");
+         row++;
+     }
+     $("#cell" + row + "_" + column).addClass("selected");
+ });
+ let row = 2;
+ let column = 2;
